d5270523481f0c3dba9aba13ddb3950fb7fa190e
Devendra Jangid <devendarjangid50@gmail.com>
2025-08-21 14:55:18 +0530

    Added isAbove fn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const age =23;
-const name = "devendra";
+const name1 = "devendra";
 const dob = "01/01/2000";
 
 function sayName() {
- console.log("My Name is " + name) 
+ console.log("My Name is " + name1) 
+}
+
+
+
+function isAbove(){
+  return age>= 18
 }
